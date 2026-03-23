8170308c765d656eecca3c3ecafb1f9ec607415a
Iskren <iskren.nemet@gmail.com>
2026-03-23 15:18:51 +0100

    feat: add Member controller and views

---
+++ spec/views/members/index.html.tailwindcss_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "members/index", type: :view do
+  before(:each) do
+    assign(:members, [
+      Member.create!(),
+      Member.create!()
+    ])
+  end
+
+  it "renders a list of members" do
+    render
+    cell_selector = 'div>p'
+  end
+end
