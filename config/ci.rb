e3ff501f1ed34cadd792f5e55f37235c6a932629
izkreny <iskren.nemet@gmail.com>
2026-04-22 11:14:06 +0200

    build: enable rspec tests in ci

--- config/ci.rb
+++ config/ci.rb
@@ -9,6 +9,7 @@ CI.run do
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
+  step "Tests: Rails", "bin/rspec"
   # step "Tests: Rails", "bin/rails test"
   # step "Tests: System", "bin/rails test:system"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
